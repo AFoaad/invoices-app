c5935be4219d8d5d0fc3cdd34774a08ca81ceac6
ahmedVo <ahmadelbana8@gmail.com>
2021-07-22 05:14:32 +0200

    adding delete modal invoice and addedthe state to store and binded with the comonent in the ui view

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <div v-if="!mobile" class="app flex flex-column">
       <Navigation />
       <div class="app-content flex flex-column">
-        <Modal />
+        <Modal v-if="modalActive"/>
         <transition name="invoice">
           <InvoiceModal v-if="invoiceModel" />
         </transition>
@@ -47,7 +47,7 @@ export default {
     },
   },
   computed: {
-    ...mapState(["invoiceModel"]),
+    ...mapState(["invoiceModel", "modalActive"]),
   },
 };
 </script>
--- src/store/index.ts
+++ src/store/index.ts
@@ -3,11 +3,15 @@ import { createStore } from "vuex";
 export default createStore({
   state: {
     invoiceModel: false,
+    ModalActive: false,
   },
   mutations: {
     TOGGLE_INVOICE(state) {
       state.invoiceModel = !state.invoiceModel;
     },
+    TOGGLE_MODAL(state){
+      state.ModalActive = !state.ModalActive;
+    }
   },
   actions: {},
   modules: {},
